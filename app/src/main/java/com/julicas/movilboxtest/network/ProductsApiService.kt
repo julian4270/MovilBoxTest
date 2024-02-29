06c9b23e5e70e324e8f29608144b6d69045542ae
julian4270 <guitarrajulian@hotmail.com>
2024-02-29 13:23:00 -0500

    Add themes

--- app/src/main/java/com/julicas/movilboxtest/network/ProductsApiService.kt
+++ app/src/main/java/com/julicas/movilboxtest/network/ProductsApiService.kt
@@ -3,20 +3,25 @@ package com.julicas.movilboxtest.network
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.julicas.movilboxtest.model.Categories
 import com.julicas.movilboxtest.model.Products
+import com.julicas.movilboxtest.model.Product
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.create
 import retrofit2.http.GET
 
     private const val BASE_URL =
-        "https://dummyjson.com/"
+        "https://dummyjson.com"
 
+/**
+ * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
+ */
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(BASE_URL)
         .build()
-
+/**
+ * Retrofit service object for creating api calls
+ */
 interface ProductsApiService {
     @GET("products")
     suspend fun getProducts(): List<Products>
---
+++ app/src/main/java/com/julicas/movilboxtest/ui/screens/ProductsViewModel.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.julicas.movilboxtest.ui.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.julicas.movilboxtest.network.ProductsApi
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+/**
+ * UI state for the Home screen
+ */
+sealed interface ProductsUiState {
+    data class Success(val products: String) : ProductsUiState
+    object Error : ProductsUiState
+    object Loading : ProductsUiState
+}
+
+class ProductsViewModel : ViewModel() {
+    /** The mutable State that stores the status of the most recent request */
+    var productsUiState: ProductsUiState by mutableStateOf(ProductsUiState.Loading)
+        private set
+
+    /**
+     * Call getProducts() on init so we can display status immediately.
+     */
+    init {
+        getProducts()
+    }
+
+    init {
+        getCategories()
+    }
+
+    /**
+     * Gets Mars photos information from the Mars API Retrofit service and updates the
+     * [MarsPhoto] [List] [MutableList].
+     */
+    fun getProducts() {
+        viewModelScope.launch {
+            productsUiState = ProductsUiState.Loading
+            productsUiState = try {
+                val listResult = ProductsApi.retrofitService.getProducts()
+                ProductsUiState.Success(
+                    "Success: ${listResult.size} Products retrieved"
+                )
+            } catch (e: IOException) {
+                ProductsUiState.Error
+            } catch (e: HttpException) {
+                ProductsUiState.Error
+            }
+        }
+    }
+
+    fun getCategories() {
+        viewModelScope.launch {
+            productsUiState = ProductsUiState.Loading
+            productsUiState = try {
+                val listResult = ProductsApi.CategoriesApi.retrofitService.getCategories()
+                ProductsUiState.Success(
+                    "Success: ${listResult.size} Products retrieved"
+                )
+            } catch (e: IOException) {
+                ProductsUiState.Error
+            } catch (e: HttpException) {
+                ProductsUiState.Error
+            }
+        }
+    }
+}
